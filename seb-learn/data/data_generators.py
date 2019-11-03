c4499f7559e4348b53718d8d6e2d605026e7e749
Sebastian Bertoli <bertoli.sebastian@mailbox.org>
2019-11-03 23:46:55 +0100

    update docstring add gen_sinusoidal_norm()

--- seb-learn/data/data_generators.py
+++ seb-learn/data/data_generators.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-def gen_sinusoidal(n, seed=None, sdev=0.2,):
+
+def gen_sinusoidal(n, seed=None, sdev=0.2):
     """Generates sinusoidal data with random noise added to it.
     
     Parameters
     ----------
     n : int
-        Number of datapoints to generate.
+        Number of data points to generate.
+
+    seed : int
+        Seed value for numpy random number generator.
+
+    sdev : float
+        Standard deviation Gaussian distribution.
     
     Returns
     -------
@@ -20,4 +27,35 @@ def gen_sinusoidal(n, seed=None, sdev=0.2,):
         np.random.seed(seed)
     x = np.linspace(0, 2 * np.pi, n).reshape([-1,1])
     t = np.random.normal(np.sin(x), sdev).reshape([-1,1])
+    return x, t
+
+
+def gen_sinusoidal_norm(n, seed=None, sdev=0.2, ):
+    """ Generates sinusoidal data with random noise added to it.
+        xi's are drawn from a uniform distribution.
+
+    Parameters
+    ----------
+    n : int
+        Number of data points to generate.
+
+    seed : int
+        Seed value for numpy random number generator.
+
+    sdev : float
+        Standard deviation Gaussian distribution.
+
+
+    Returns
+    -------
+    x : Numpy array [n, 1]
+        Feature vector.
+
+    t : Numpy array [n, 1]
+        Target vector.
+    """
+    if seed:
+        np.random.seed(seed)
+    x = np.random.uniform(0, 2 * np.pi, n).reshape([-1, 1])
+    t = np.random.normal(np.sin(x), sdev).reshape([-1, 1])
     return x, t
